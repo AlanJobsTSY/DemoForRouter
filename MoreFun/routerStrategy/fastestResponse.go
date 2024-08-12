078a418bab17a5b75f184a62a5053fa9bd5ef072
shenytong <shenytong@tencent.com>
2024-08-12 19:45:20 +0800

    2024/8/12

--- MoreFun/routerStrategy/fastestResponse.go
+++ MoreFun/routerStrategy/fastestResponse.go
@@ -6,39 +6,52 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"strings"
+	"sync"
 	"time"
 )
 
 func fastestResponse(myServicesStorage *ServicesStorage, svrName string) string {
 	var addr string
 	var minn = time.Duration(1<<63 - 1)
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
 	myServicesStorage.RLock()
 	defer myServicesStorage.RUnlock()
+
 	if instances, ok := myServicesStorage.ServicesStorage[svrName]; ok && len(instances) > 0 {
 		for _, instanceAddr := range instances {
 			parts := strings.Split(instanceAddr, ":")
-			// 获取实例的权重
 			addrInstance := fmt.Sprintf("%s:%s", parts[1], parts[2])
-			startTime := time.Now()
 
-			ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
-			defer cancel()
-			// 创建 gRPC 连接，使用 grpc.WithBlock() 确保连接完全建立
-			conn, err := grpc.DialContext(ctx, addrInstance, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+			wg.Add(1)
+			// 用协程可以大大缩短大批量的连接时间计算
+			go func(addrInstance string) {
+				defer wg.Done()
+				//记录开始时间
+				startTime := time.Now()
+				ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+				defer cancel()
 
-			if err != nil {
-				continue
-			}
-			// 记录结束时间
-			endTime := time.Now()
-			conn.Close()
-			connectionTime := endTime.Sub(startTime)
-			//log.Printf("%d", connectionTime)
-			if minn > connectionTime {
-				minn = connectionTime
-				addr = addrInstance
-			}
+				conn, err := grpc.DialContext(ctx, addrInstance, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+				if err != nil {
+					return
+				}
+				defer conn.Close()
+				//记录结束时间
+				endTime := time.Now()
+				connectionTime := endTime.Sub(startTime)
+				// 保护minn和addr这两个共享变量
+				mu.Lock()
+				defer mu.Unlock()
+				if connectionTime < minn {
+					minn = connectionTime
+					addr = addrInstance
+				}
+			}(addrInstance)
 		}
+		wg.Wait()
 	}
+
 	return addr
 }
