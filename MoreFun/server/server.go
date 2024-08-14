828f361914bc5d2ac727425aed4c5a18d1a5060a
shenytong <shenytong@tencent.com>
2024-08-14 17:53:54 +0800

    2024/8/13.1

--- MoreFun/server/server.go
+++ MoreFun/server/server.go
@@ -49,7 +49,7 @@ func (s *MiniGameRouterServer) SayHello(ctx context.Context, req *pb.HelloReques
 func startGRPCServer(port int) {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Printf("Failed to listen: %v\n", err)
+		log.Fatalf("Failed to listen: %v\n", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterMiniGameRouterServer(s, &MiniGameRouterServer{port: port, times: 1})
