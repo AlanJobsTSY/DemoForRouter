dd4fa87b4daeb88a9b630717afd553b86ac08d43
shenytong <shenytong@tencent.com>
2024-08-16 21:57:47 +0800

    2024/8/15

--- MoreFun/map.go
+++ MoreFun/map.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
-	"runtime/pprof"
 	"time"
 )
 
@@ -12,24 +10,8 @@ var cnt = map[int]int{1: 0, 2: 0, 3: 0}
 
 func main() {
 
-	// 创建 CPU 分析文件
-	f, err := os.Create("cpu.pprof")
-	if err != nil {
-		log.Fatal("could not create CPU profile: ", err)
+	for {
+		log.Printf("1")
+		time.Sleep(5 * time.Second)
 	}
-	defer f.Close()
-
-	// 开始 CPU 分析
-	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Fatal("could not start CPU profile: ", err)
-	}
-	defer pprof.StopCPUProfile()
-
-	// 模拟一些工作负载
-	for i := 0; i < 10; i++ {
-		time.Sleep(100 * time.Millisecond)
-	}
-
-	t := time.Now()
-	log.Printf("%v", t)
 }
