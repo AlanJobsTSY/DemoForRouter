1b8ef5abfc3f75d75693ee9d391067590d13f0e3
shenytong <shenytong@tencent.com>
2024-08-16 21:09:20 +0800

    2024/8/15

---
+++ MoreFun/Start_B.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 可执行文件的路径
+executable="go run ./server/server.go"
+
+# 初始参数
+initial_port=40000
+
+# 执行次数
+num_executions=200
+
+# 服务器的其他参数
+name="B"
+ip="9.135.119.71"
+weight=5
+
+for ((i=1; i<=num_executions; i++))
+do
+    port=$((initial_port + (i - 1) * 2))
+    echo "Starting execution $i with port $port"
+    $executable --name $name --ip $ip --port $port --weight $weight &
+done
+
+echo "All instances started."
