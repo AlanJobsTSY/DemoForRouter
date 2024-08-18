a25f9e2a13b4c55e0ef3ad1dbeebee3d30064b1a
shenytong <shenytong@tencent.com>
2024-08-18 20:52:48 +0800

    2024/8/17.2

--- MoreFun/routerStrategy/leastconnections.go
+++ MoreFun/routerStrategy/leastconnections.go
@@ -5,11 +5,9 @@ import (
 	"context"
 	"fmt"
 	clientv3 "go.etcd.io/etcd/client/v3"
-	"log"
 	"math"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func leastConnections(myServicesStorage *ServicesStorage, svrName string) string {
@@ -21,7 +19,6 @@ func leastConnections(myServicesStorage *ServicesStorage, svrName string) string
 	var addr string
 	var key string
 	if instances, ok := myServicesStorage.ServicesStorage[svrName]; ok && len(instances) > 0 {
-		startTime := time.Now()
 		for k, v := range instances {
 			parts := strings.Split(v, ":")
 			partConn, _ := strconv.Atoi(parts[6])
@@ -36,8 +33,7 @@ func leastConnections(myServicesStorage *ServicesStorage, svrName string) string
 		parts[6] = strconv.Itoa(minnConn + 1)
 		// 将 parts 切片重新拼接成字符串
 		newValue := strings.Join(parts, ":")
-		endTime := time.Now()
-		log.Printf("%d", endTime.Sub(startTime).Microseconds())
+
 		_, err := cli.Put(context.Background(), key, newValue, clientv3.WithIgnoreLease())
 		if err != nil {
 			fmt.Println("Failed to update etcd:", err)
