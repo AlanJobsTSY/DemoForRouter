1d040851c9a16129e26bb89e6a3800cf0b4353a8
shenytong <shenytong@tencent.com>
2024-08-18 21:10:12 +0800

    2024/8/17.2

--- MoreFun/routerStrategy/weightedLeastConnections.go
+++ MoreFun/routerStrategy/weightedLeastConnections.go
@@ -18,15 +18,15 @@ func weightedLeastConnections(myServicesStorage *ServicesStorage, svrName string
 	var addr string
 	if instances, ok := myServicesStorage.ServicesStorage[svrName]; ok && len(instances) > 0 {
 		minnConn := math.MaxInt
-		minnWeight := 0
+		//minnWeight := 0
 		var key string
 		for k, v := range instances {
 			parts := strings.Split(v, ":")
 			partConn, _ := strconv.Atoi(parts[6])
 			//partWeight, _ := strconv.Atoi(parts[4])
-			if minnConn*1 >= partConn*minnWeight {
+			if minnConn >= partConn {
 				minnConn = partConn
-				minnWeight = 1
+				//minnWeight = 1
 				addr = fmt.Sprintf("%s:%s", parts[1], parts[2])
 				key = k
 			}
