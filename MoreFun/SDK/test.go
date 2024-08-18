52ea4fd170b644448c45ced65fc56bcc9ac610fd
shenytong <shenytong@tencent.com>
2024-08-18 16:45:36 +0800

    2024/8/17.2

--- MoreFun/SDK/test.go
+++ MoreFun/SDK/test.go
@@ -76,7 +76,7 @@ func testFixedTypeRouting(endPoint *endPoint.EndPoint, client *pb.MiniGameRouter
 		fmt.Println("Invalid number")
 		return
 	}
-	limiter := rate.NewLimiter(500, 500)
+	limiter := rate.NewLimiter(4000, 4000)
 	var wg sync.WaitGroup
 	var mu sync.Mutex
 	// 记录开始时间
