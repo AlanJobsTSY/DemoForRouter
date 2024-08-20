69080f107ab2ce3d2c375d55063720428e08b358
shenytong <shenytong@tencent.com>
2024-08-20 12:37:09 +0800

    2024/8/19

--- MoreFun/routerStrategy/weightedLeastConnections.go
+++ MoreFun/routerStrategy/weightedLeastConnections.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"fmt"
 	clientv3 "go.etcd.io/etcd/client/v3"
-	"log"
-	"math"
 	"strconv"
 	"strings"
 )
@@ -18,17 +16,17 @@ func weightedLeastConnections(myServicesStorage *ServicesStorage, svrName string
 	defer myServicesStorage.RUnlock()
 	var addr string
 	if instances, ok := myServicesStorage.ServicesStorage[svrName]; ok && len(instances) > 0 {
-		minnConn := math.MaxInt
-		minnWeight := 1
+		minnConn := 10000
+		minnWeight := 0
 		var key string
 		for k, v := range instances {
 			parts := strings.Split(v, ":")
 			partConn, _ := strconv.Atoi(parts[6])
 			partWeight, _ := strconv.Atoi(parts[4])
-			log.Printf("%d %d %d %d", minnConn, minnWeight, partConn, partWeight)
-			log.Printf("%d %d", minnConn*partWeight, partConn*minnWeight)
+			//log.Printf("%d %d %d %d", minnConn, minnWeight, partConn, partWeight)
+			//log.Printf("%d %d", minnConn*partWeight, partConn*minnWeight)
 			if minnConn*partWeight >= partConn*minnWeight {
-				log.Printf("change")
+				//log.Printf("change")
 				minnConn = partConn
 				minnWeight = partWeight
 				addr = fmt.Sprintf("%s:%s", parts[1], parts[2])
